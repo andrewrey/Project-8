9d116d1557062d741aa1366196b47f803faf2b7e
Andrew Reynolds <andrew@nearablecorp.com>
2020-03-29 22:05:05 -0700

    started on async function for loading cards data

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+
+const empUrl = "https://randomuser.me/api/?results=12"
+const cardHolder = document.querySelector('.card-holder');
+const employees = [];
+
+async function getEmployees(url){
+
+
+
+}
+
+
